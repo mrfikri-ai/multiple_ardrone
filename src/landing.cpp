9ab5c7b0f684897e1ad76ba85c3728d740e63da5
Alphaproxima <Alphaproxima@users.noreply.github.com>
2015-05-10 08:47:22 +0700

    Update landing.cpp

--- src/landing.cpp
+++ src/landing.cpp
@@ -9,7 +9,37 @@
 
 std_msgs::Empty landing;
 
-int main(int argc, char** argv);
+int main(int argc, char** argv)
 {
-
-}
+ 
+ ros::init (argc, argv, "Multiple_ARDrone_Test");
+ ros::NodeHandle node_handle;
+ ros::Rate rate(50);
+ 
+ ros::Publisher a,b;
+ a = node_handle.advertise<std_msgs::Empty>("/ardrone1/ardrone/land", 1); commence to make ardrone 1 land
+ b = node_handle.advertise<std_msgs::Empty>("/ardrone2/ardrone/land", 1);
+ 
+ float land_time = 5.0;
+ 
+ while(ros::ok())
+ {
+  
+  //using ros::Time::now() as timer when ardrone takeoff and landing as well
+  double time = (double)ros::Time::now().toSec();
+  while((double)ros::Time::now().toSec() < time + land_time)
+  {
+   
+   a.publish(landing);
+   b.publish(landing);
+   ros::spinOnce();
+   rate.sleep();
+   
+  } // landing while
+  
+  ROS_INFO("AR.Drone 1 and 2 landing");
+  exit(0);
+  
+ } // while loop
+ 
+} //main program
