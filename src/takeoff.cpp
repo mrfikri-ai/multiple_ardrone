5465b740f8f648f8c26691fefed2c37cbf338f03
Alphaproxima <Alphaproxima@users.noreply.github.com>
2015-05-10 08:34:36 +0700

    Update takeoff.cpp
    declare a and b as ardrone1 and ardrone2 topic to be controlled with this file

--- src/takeoff.cpp
+++ src/takeoff.cpp
@@ -1,4 +1,41 @@
+/*  Program for takeoff between two ardrones
+ *  Muhamad Rausyan Fikri - UGM 
+ *
+ *  Elins research group
+ */
+
 #include <ros/ros.h>
 #include <std_msgs/Empty.h>
 
-ros::Publisher a,b,c;
+std_msgs::Empty takeoff;
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "Multiple_ARDrone_Test");
+  ros::NodeHandle node_handle;
+  ros::Rate rate(50);
+  
+  ros::Publisher a,b;
+  
+  //based on rqt when controlling 2 ardrones, the topics have a name for each ardrone
+  //it depends on how many ardrone ip that connected with your access point and ROS
+  a = node_handle.advertise<std_msgs::Empty>("/ardrone1/ardrone/takeoff", 1);
+  b = node_handle.advertise<std_msgs::Empty>("/ardrone2/ardrone/takeoff", 1);
+  float takeoff_time = 5.0;
+  
+  while(ros::ok())
+  {
+    double time = (double)ros::Time::now().toSec();
+    while ((double)ros::Time::now().toSec() < time + takeoff_time)
+    {
+      a.publish(takeoff);
+      b.publish(takeoff);
+      ros::spinOnce();
+      rate.sleep();
+    }
+    
+    ROS_INFO("AR.Drone Takeoff");
+    exit(0);
+  }// while 
+  
+}// main
