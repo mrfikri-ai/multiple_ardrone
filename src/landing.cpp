4bcc3a0c01dc409bfed267da5813ed4918114f77
Alphaproxima <Alphaproxima@users.noreply.github.com>
2015-05-10 08:37:52 +0700

    Create landing.cpp

---
+++ src/landing.cpp
@@ -0,0 +1,15 @@
+/*  Program for takeoff between two ardrones
+ *  Muhamad Rausyan Fikri - UGM 
+ *
+ *  Elins research group
+ */
+ 
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+
+std_msgs::Empty landing;
+
+int main(int argc, char** argv);
+{
+
+}
