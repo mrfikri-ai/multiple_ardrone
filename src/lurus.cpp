01c70994e5a38028a35bfd02413d8aa25a66d036
Alphaproxima <Alphaproxima@users.noreply.github.com>
2015-05-11 12:39:55 +0700

    Create lurus.cpp

---
+++ src/lurus.cpp
@@ -0,0 +1,13 @@
+
+
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
+
+std_msgs::Empty takeoff, landing;
+geometry_msgs::Twist lurus;
+
+int main(int argc, char** argv)
+{
+
+}
