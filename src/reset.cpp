25ff16a52f93862c0e61d09728420a6801711682
Alphaproxima <Alphaproxima@users.noreply.github.com>
2015-05-11 08:30:23 +0700

    Create reset.cpp

---
+++ src/reset.cpp
@@ -0,0 +1,12 @@
+/*  Program for takeoff between two ardrones
+ *  Muhamad Rausyan Fikri - UGM 
+ *
+ *  Elins research group
+ *  Reset.cpp
+ */
+ 
+ #include <ros/ros.h>
+ #include <std_msgs/Empty.h>
+ std_msgs::Empty res;
+ 
+ 
