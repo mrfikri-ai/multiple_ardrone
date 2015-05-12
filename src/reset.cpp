ffdb6d4130c2c36d3bfdd566b99c3e1f9c0416bd
Alphaproxima <Alphaproxima@users.noreply.github.com>
2015-05-12 09:38:31 +0700

    Update reset.cpp

--- src/reset.cpp
+++ src/reset.cpp
@@ -5,8 +5,11 @@
  *  Reset.cpp
  */
  
- #include <ros/ros.h>
- #include <std_msgs/Empty.h>
- std_msgs::Empty res;
- 
- 
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <cstdlib>
+#include <ardrone_autonomy/Navdata.h>
+
+std_msgs::Empty res;
+ardrone_autonomy::Navdata msg_in;
+int drone_state;
