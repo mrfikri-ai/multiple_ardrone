888e446a86b2ff1190def5b2c85f213d914bc55f
Muhamad Rausyan Fikri <Alphaproxima@users.noreply.github.com>
2015-06-20 10:04:36 +0700

    Update keyex.cpp

--- src/keyex.cpp
+++ src/keyex.cpp
@@ -17,10 +17,7 @@
 #include <cmath>
 #include <termios.h>
 
-#define PI 3.14
-
 using namespace std;
-
 
 int getche(void) {
     struct termios oldattr, newattr;
@@ -52,6 +49,7 @@ void ambilchar()
 {
 m = getche();
 }
+
 int main(int argc, char** argv)
 {
 
@@ -80,93 +78,93 @@ int main(int argc, char** argv)
 */
 
 //hover message
-			hover.linear.x=0.0; 
-			hover.linear.y=0.0;
-			hover.linear.z=0.0;
-			hover.angular.x=0.0; 
-			hover.angular.y=0.0;
-			hover.angular.z=0.0;  
+	hover.linear.x=0.0; 
+	hover.linear.y=0.0;
+	hover.linear.z=0.0;
+	hover.angular.x=0.0; 
+	hover.angular.y=0.0;
+	hover.angular.z=0.0;  
 			
 //rotasi kanan 180 derajat
-			rotasi.linear.x = 0.0;
-			rotasi.linear.y = 0.0;
-			rotasi.linear.z = 0.0;
-			rotasi.angular.x = 0.0;
-			rotasi.angular.y = 0.0;
-			rotasi.angular.z = 0.5;
+	rotasi.linear.x = 0.0;
+	rotasi.linear.y = 0.0;
+	rotasi.linear.z = 0.0;
+	rotasi.angular.x = 0.0;
+	rotasi.angular.y = 0.0;
+	rotasi.angular.z = 0.5;
 
 //rotasi kiri 180 derajat
-			yaw.linear.x = -rotasi.linear.x;
-			yaw.linear.y = -rotasi.linear.y;
-			yaw.linear.z = -rotasi.linear.z;
-			yaw.angular.x = -rotasi.angular.x;
-			yaw.angular.y = -rotasi.angular.y;
-			yaw.angular.z = -rotasi.angular.z;
+	yaw.linear.x = -rotasi.linear.x;
+	yaw.linear.y = -rotasi.linear.y;
+	yaw.linear.z = -rotasi.linear.z;
+	yaw.angular.x = -rotasi.angular.x;
+	yaw.angular.y = -rotasi.angular.y;
+	yaw.angular.z = -rotasi.angular.z;
 			
 //up message
-			naik.linear.x=0.0; 
-			naik.linear.y=0.0;
-			naik.linear.z=0.05;
-			naik.angular.x=0.0; 
-			naik.angular.y=0.0;
-			naik.angular.z=0.0;
+	naik.linear.x=0.0; 
+	naik.linear.y=0.0;
+	naik.linear.z=0.05;
+	naik.angular.x=0.0; 
+	naik.angular.y=0.0;
+	naik.angular.z=0.0;
 
 //turun
-			turun.linear.x = -naik.linear.x;
-			turun.linear.y = -naik.linear.y;
-			turun.linear.z = -naik.linear.z;
-			turun.angular.x = -naik.angular.x;
-			turun.angular.y = -naik.angular.y;
-			turun.angular.z = -naik.angular.z;	
+	turun.linear.x = -naik.linear.x;
+	turun.linear.y = -naik.linear.y;
+	turun.linear.z = -naik.linear.z;
+	turun.angular.x = -naik.angular.x;
+	turun.angular.y = -naik.angular.y;
+	turun.angular.z = -naik.angular.z;	
 
 //kiri 
-			kiri.linear.x = 0.0;
-			kiri.linear.y = 0.1;
-			kiri.linear.z = 0.0;
-			kiri.angular.x = 0.0;
-			kiri.angular.y = 0.0;
-			kiri.angular.z = 0.0;
+	kiri.linear.x = 0.0;
+	kiri.linear.y = 0.1;
+	kiri.linear.z = 0.0;
+	kiri.angular.x = 0.0;
+	kiri.angular.y = 0.0;
+	kiri.angular.z = 0.0;
 			
 //kanan
-			kanan.linear.x = -kiri.linear.x;
-			kanan.linear.y = -kiri.linear.y;
-			kanan.linear.z = -kiri.linear.z;
-			kanan.angular.x = -kiri.angular.x;
-			kanan.angular.y = -kiri.angular.y;
-			kanan.angular.z = -kiri.angular.z;
+	kanan.linear.x = -kiri.linear.x;
+	kanan.linear.y = -kiri.linear.y;
+	kanan.linear.z = -kiri.linear.z;
+	kanan.angular.x = -kiri.angular.x;
+	kanan.angular.y = -kiri.angular.y;
+	kanan.angular.z = -kiri.angular.z;
 			
 //gerak melingkar ccw			
-			lingkar.linear.x=0.2; 
-			lingkar.linear.y=0.0;
-			lingkar.linear.z=0.0;
-			lingkar.angular.x=0.0; 
-			lingkar.angular.y=0.0;
-			lingkar.angular.z=0.2;
+	lingkar.linear.x=0.2; 
+	lingkar.linear.y=0.0;
+	lingkar.linear.z=0.0;
+	lingkar.angular.x=0.0; 
+	lingkar.angular.y=0.0;
+	lingkar.angular.z=0.2;
 
 //gerak melingkar cw
-			lingkar_neg.linear.x= lingkar.linear.x; 
-			lingkar_neg.linear.y=-lingkar.linear.y;
-			lingkar_neg.linear.z=-lingkar.linear.z;
-			lingkar_neg.angular.x=-lingkar.angular.x; 
-			lingkar_neg.angular.y=-lingkar.angular.y;
-			lingkar_neg.angular.z=-lingkar.angular.z;
+	lingkar_neg.linear.x= lingkar.linear.x; 
+	lingkar_neg.linear.y=-lingkar.linear.y;
+	lingkar_neg.linear.z=-lingkar.linear.z;
+	lingkar_neg.angular.x=-lingkar.angular.x; 
+	lingkar_neg.angular.y=-lingkar.angular.y;
+	lingkar_neg.angular.z=-lingkar.angular.z;
 
 // gerak lurus
-			lurus.linear.x=0.1; 
-			lurus.linear.y=0.0;
-			lurus.linear.z=0.0;
-			lurus.angular.x=0.0; 
-			lurus.angular.y=0.0;
-			lurus.angular.z=0.0;
+	lurus.linear.x=0.1; 
+	lurus.linear.y=0.0;
+	lurus.linear.z=0.0;
+	lurus.angular.x=0.0; 
+	lurus.angular.y=0.0;
+	lurus.angular.z=0.0;
 
 //gerak mundur
-			mundur.linear.x=-lurus.linear.x; 
-			mundur.linear.y=-lurus.linear.y;
-			mundur.linear.z=-lurus.linear.z;
-			mundur.angular.x=-lurus.angular.x; 
-			mundur.angular.y=-lurus.angular.y;
-			mundur.angular.z=-lurus.angular.z;
-	
+	mundur.linear.x=-lurus.linear.x; 
+	mundur.linear.y=-lurus.linear.y;
+	mundur.linear.z=-lurus.linear.z;
+	mundur.angular.x=-lurus.angular.x; 
+	mundur.angular.y=-lurus.angular.y;
+	mundur.angular.z=-lurus.angular.z;
+
 	pub_twist1 = node.advertise<geometry_msgs::Twist>("/ardrone1/cmd_vel", 1); /* Message queue length is just 1 */
 	pub_twist2 = node.advertise<geometry_msgs::Twist>("/ardrone2/cmd_vel", 1); /* Message queue length is just 1 */
 	pub_empty_takeoff1 = node.advertise<std_msgs::Empty>("/ardrone1/ardrone/takeoff", 1); /* Message queue length is just 1 */
@@ -177,28 +175,31 @@ int main(int argc, char** argv)
 	pub_empty_reset2 = node.advertise<std_msgs::Empty>("/ardrone2/ardrone/reset", 1); /* Message queue length is just 1 */
 
 //	start_time =(double)ros::Time::now().toSec();	
-	ROS_INFO("AR Drone loop dimulai, biasanya loop forever");
+	ROS_INFO("AR Drone loop dimulai");
 
-//	int n=360;
-	float y, xy;
-	float radius;
-	float iterasi = 0.0;
-	
-	xy = 2*PI;
-	
-	cout <<"berapa kali putaran \n";
-	cin >> y;
-	cout <<"Input radius lingkaran \n";
-	cin >> radius;
-	cout <<" banyaknya putaran yang diinginkan = \n "<<y*xy<<"radius = \n "<<radius;
-	
-getchar();
+// kecepatan linearnya 0.2 m/s
+float pi = 3.1415;
+float lin_speed = 0.2;
+float goal_lin;
+cout<<"jarak yang ingin ditempuh =";
+cin>> goal_lin;
+
+// waktu yang ditempuh untuk jarak x meter
+float lin_duration = goal_lin/lin_speed;
+
+// menentukan kecepatan angular 1 radian per detik
+float ang_speed = 1.0;	
+// goal dari 1 lingkaran itu 1 pi
+float goal_ang = pi;
+//durasi tempuh 1 lingkaran
+float durasi = goal_ang/ang_speed;
+
 
 while (ros::ok()) 
 {
 	//Definisi dari banyaknya iterasi yang diinginkan
 		
-	cout <<" simple keyboard controller yang digunakan untuk mengendalikan dua quadrotor \n"
+	cout	 <<" simple keyboard controller yang digunakan untuk mengendalikan dua quadrotor \n"
 		 <<" inilah perintah yang digunakan untuk setiap stepnya \n"
 		 <<" =========================================================================== \n"
 		 <<" t/l = takeoff / landing \n"
@@ -242,8 +243,6 @@ while (ros::ok())
 		if(m == 'r')
 		{				
 			ROS_INFO("Closing Node");
-			//pub_empty_reset1.publish(msg); //kills the drone
-			//pub_empty_reset2.publish(msg); //kills the drone		
 			exit(0); 
 			ros::spinOnce();
 			loop_rate.sleep();
@@ -254,7 +253,7 @@ while (ros::ok())
 		if(m == 'a')
 		{
 			ROS_INFO("Kiri");
-			kiri.linear.y += 0.1;
+			kiri.linear.y = 0.1;
 			pub_twist1.publish(kiri);
 			pub_twist2.publish(kiri);
 			m = ' ';
@@ -264,7 +263,7 @@ while (ros::ok())
 		if(m == 'd')
 		{
 			ROS_INFO("kanan");
-			kanan.linear.y -= 0.1;
+			kanan.linear.y = -0.1;
 			pub_twist1.publish(kanan);
 			pub_twist2.publish(kanan);
 			m = ' ';
@@ -282,28 +281,42 @@ while (ros::ok())
 //Perintah untuk maju		
 		if(m == 'w' )
 		{
-			ROS_INFO("maju");
-			lurus.linear.x += 0.1;
-			pub_twist1.publish(lurus);
-			pub_twist2.publish(lurus);
-			m = ' ';
+			for(int i=0; i<lin_duration; i++)
+			{
+	
+				ROS_INFO("maju");
+				lurus.linear.x = lin_speed;
+				pub_twist1.publish(lurus);
+				pub_twist2.publish(lurus);
+				loop_rate.sleep();
+				m = ' ';
+			}
+		pub_twist1.publish(hover);
+		pub_twist2.publish(hover);
 		}
 
 //Perintah untuk mundur		
-		if(m == 's' )
+		if(m == 's')
 		{
-			ROS_INFO("mundur");
-			mundur.linear.x -= 0.1;
-			pub_twist1.publish(mundur);
-			pub_twist2.publish(mundur);
-			m = ' ';
+			for(int i = 0; i<lin_duration; i++)
+			{
+				ROS_INFO("mundur");
+				mundur.linear.x = -lin_speed;
+				pub_twist1.publish(mundur);
+				pub_twist2.publish(mundur);
+				loop_rate.sleep();
+				m = ' ';
+			}
+			pub_twist1.publish(hover);
+			pub_twist2.publish(hover);	
 		}
 
 //Perintah yawing ke kanan		
 		if(m == 'q' )
 		{
+			for(int i=0)
 			ROS_INFO("yawing ke kanan");
-			rotasi.angular.z += 0.1;
+			rotasi.angular.z = lin_speed;
 			pub_twist1.publish(rotasi);
 			pub_twist2.publish(rotasi);
 			m = ' ';
@@ -313,7 +326,7 @@ while (ros::ok())
 		if(m == 'e' )
 		{
 			ROS_INFO("yawing ke kiri");
-			yaw.angular.z -= 0.5;
+			yaw.angular.z = -0.5;
 			pub_twist1.publish(yaw);
 			pub_twist2.publish(yaw);
 			m = ' ';
@@ -323,7 +336,7 @@ while (ros::ok())
 		if(m == 'i' )
 		{
 			ROS_INFO("naik");
-			naik.linear.z += 0.05;
+			naik.linear.z = 0.05;
 			pub_twist1.publish(naik);
 			pub_twist2.publish(naik);
 			m = ' ';
@@ -333,7 +346,7 @@ while (ros::ok())
 		if(m == 'k' )
 		{
 			ROS_INFO("turun");
-			turun.linear.z -= 0.05;
+			turun.linear.z = -0.05;
 			pub_twist1.publish(turun);
 			pub_twist2.publish(turun);
 			m = ' ';
@@ -352,8 +365,8 @@ while (ros::ok())
 			ROS_INFO("lingkar cw");
 			for(iterasi = 0; iterasi<=10); iterasi +=1)
 			{
-				lingkar.linear.x += 0.2;
-				lingkar.angular.z += (lingkar.linear.x/radius) ;
+				lingkar.linear.x = 0.2;
+				lingkar.angular.z = (lingkar.linear.x/radius) ;
 				pub_twist1.publish(lingkar);
 				pub_twist2.publish(lingkar);
 				m = ' ';	
@@ -363,13 +376,13 @@ while (ros::ok())
 //Perintah untuk menggerakkan AR.Drone negatif. Negatif bukan menandakan besaran tapi arah		
 		if(m == 'p' )
 		{
-			lingkar_neg.linear.x = 0.2;
-			lingkar_neg.angular.z = (lingkar.angular.z/radius);
+			lingkar_neg.linear.x = 0.0;
+			lingkar_neg.angular.z = 0.0;
 			ROS_INFO("lingkar ccw");
 			for(iterasi=0; iterasi<=10; iterasi +=1)
 			{
-				lingkar_neg.linear.x += 0.1;  //kecepatan linearnya
-				lingkar_neg.angular.z -= ((lingkar.linear.x)/radius); //kecepatan angular
+				lingkar_neg.linear.x = 0.2;  //kecepatan linearnya
+				lingkar_neg.angular.z = -((lingkar.linear.x)/radius); //kecepatan angular
 				pub_twist1.publish(lingkar_neg);
 				pub_twist2.publish(lingkar_neg);
 				m = ' ';
@@ -381,7 +394,7 @@ while (ros::ok())
 		{	
 			iterasi = 0;		
 			lingkar.linear.x = 0.0;
-			lingkar.angular.z = (lingkar.angular.z/radius);
+			lingkar.angular.z = 0.0;
 			ROS_INFO("angka delapan");
 			for(iterasi=0; iterasi<=10); iterasi +=1)
 			{
@@ -391,7 +404,7 @@ while (ros::ok())
 				pub_twist2.publish(lingkar);
 				m = ' ';	
 			}
-			
+			cout<<"iterasi = "<<iterasi;
 			for(iterasi=10; iterasi>=0; iterasi -=1)
 			{
 				lingkar_neg.linear.x = 0.2;
